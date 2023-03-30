7168a8aff1bf426b9802f3752fc1f7f690c64436
Thecafremo <jorgepardopeset@gmail.com>
2023-03-30 23:17:22 +0200

    Appropriately set `Package`.

--- Package.swift
+++ Package.swift
@@ -3,26 +3,11 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "MDKit",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MDKit",
-            targets: ["MDKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MDKit",
-            dependencies: []),
-        .testTarget(
-            name: "MDKitTests",
-            dependencies: ["MDKit"]),
-    ]
-)
+let package = Package(name: "MagicDealersKit",
+                      platforms: [.iOS(.v16)],
+                      products: [.library(name: "MDKit",
+                                          type: nil,
+                                          targets: ["MDKit"])],
+                      targets: [.target(name: "MDKit"),
+                                .testTarget(name: "MDKitTests",
+                                            dependencies: ["MDKit"])])
